b0473953d079698e2c989b4df27017f07d702824
Madina-kh16 <madinahacieva007@mail.ru>
2024-03-16 17:08:53 +0300

    deleted the file

--- pages/src/index.js
+++ pages/src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Block from "./Block";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-const blocks = ReactDOM.createRoot(document.getElementById("blocks__ads"));
-blocks.render(<Block />);
